9e75a3a5761671f71dddc95b6ab84850f4d06d53
rkvrpl <rkvrpl@icloud.com>
2023-11-23 18:50:31 +0300

    done hw

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,25 @@
 "use strict"
 
-const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъьэюя";
+const button = document.querySelector('#button');
+const text = document.querySelector('#text');
 
 const getRandomText = () =>{
+const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъьэюя";
 const indexOne = Math.floor(Math.random() * alphabet.length);
 const indexTwo = Math.floor(Math.random() * alphabet.length);
 const indexThree = Math.floor(Math.random() * alphabet.length);
 const indexFour = Math.floor(Math.random() * alphabet.length);
-
+const randomLetterOne = alphabet[indexOne];
+const randomLetterTwo = alphabet[indexTwo];
+const randomLetterThree = alphabet[indexThree];
+const randomLetterFour = alphabet[indexFour];
+const randomText = randomLetterOne + randomLetterTwo + randomLetterThree + randomLetterFour;
+text.textContent = randomText; 
 };
+button.addEventListener('click', getRandomText);
 
 
-// 3. Используя полученные случайные индексы, извлеки соответствующие символы из `alphabet` и объедини их в строку, чтобы сформировать случайное слово.
-// 4. Выведи полученное случайное слово на экран, чтобы проверить результат.
-
-
-
+{
 let text = "у лукоморья клён зелёный; златая цепь на клён том; и днём и ночью кот учёный; всё ходит по цепи кругом; идёт направо — песнь заводит; налево — сказку говорит; там чудеса: там леший бродит; русалка на ветвях сидит; там на неведомых дорожках; следы невиданных зверей; избушка там на курьих ножках; стоит без окон, без дверей; там лес и дол видений полны; там о заре прихлынут волны; на брег песчаный и пустой; и тридцать витязей прекрасных; чредой из вод выходят ясных; и с ними дядька их морской; там королевич мимоходом; пленяет грозного царя; там в облаках перед народом; через леса, через моря; колдун несёт богатыря; в темнице там царевна тужит; а бурый волк ей верно служит; там ступа с бабою ягой; идёт, бредёт сама собой; там царь кащей над златом чахнет; там русский дух… там русью пахнет!; и там я был, и мёд я пил; у моря видел клён зелёный; под ним сидел, и кот учёный; свои мне сказки говорил."
 
 const result = text.replace(/;/g, "\n");
@@ -36,3 +40,4 @@ console.log(`6. Строка result в верхнем регистре: \n ${rep
 console.log(`7. Замена "клён" на "дуб" (все вхождения) в переменной result: \n ${replacedText3}`);
 console.log(`8. Индекс первого вхождения "моря": ${index}`);
 console.log(`9. Строка с измененной первой буквой: ${modifiedText}`);
+}
